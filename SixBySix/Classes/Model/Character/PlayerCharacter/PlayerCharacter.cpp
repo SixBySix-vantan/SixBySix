33ebfdb85c0882c1ff7bb85c4c299f181cec1c22
IchirotaYasuda <beepeep777@gmail.com>
2016-07-16 14:17:00 +0900

    playerChara仮

---
+++ SixBySix/Classes/Model/Character/PlayerCharacter/PlayerCharacter.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayerCharacter.cpp
+//  SixBySix
+//
+//  Created by 安田一朗太 on 2016/07/16.
+//
+//
+
+#include "PlayerCharacter.hpp"
---
+++ SixBySix/Classes/Model/Character/PlayerCharacter/PlayerCharacter.hpp
@@ -0,0 +1,14 @@
+//
+//  PlayerCharacter.hpp
+//  SixBySix
+//
+//  Created by 安田一朗太 on 2016/07/16.
+//
+//
+
+#ifndef PlayerCharacter_hpp
+#define PlayerCharacter_hpp
+
+#include <stdio.h>
+
+#endif /* PlayerCharacter_hpp */
